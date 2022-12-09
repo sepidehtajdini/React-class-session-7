eca130df00b24e40c13e3ce5075f1e81af977cc7
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2022-12-09 11:25:52 +0330

    Update Post.js

--- 7/content/Post/Post.js
+++ 7/content/Post/Post.js
@@ -12,30 +12,13 @@ function Post({ id, title, body, bookmarkId, setBookmarkId, searchText }) {
     if (searchText !== "" && title.includes(searchText) || body.includes(searchText)) {
         foundClass = 'post--found';
     }
-    function foundInBody(str) {
-        if (body.includes(str)) {
-            // const words =body.split(" ");
-            // const start = words.toString().indexOf(str);
-            return `found ${str} in body`
-        }
-        else { return "" }
-    }
-    function founInTitle(str) {
-        if (title.includes(str)) {
-            return `found ${str} in title`
-        }
-        else {
-            return ""
-        }
-    }
+    const titleHighlight = title.replace(searchText, `<strong style="color:red">${searchText}</strong>`)
+    const bodyHighlight = body.replace(searchText, `<strong style="color:red">${searchText}</strong>`)
     return (
         <div className={`post ${bookmarkClass} ${foundClass}`} >
             <div className="post__header">
                 <span className="post__title" dangerouslySetInnerHTML={{
-                    __html:
-                        `<div class="highlight">
-           ${founInTitle(searchText)}
-           </div> ${title}`
+                    __html: titleHighlight
                 }} />
                 <Button
                     className="bookmark primary_btn"
@@ -43,10 +26,7 @@ function Post({ id, title, body, bookmarkId, setBookmarkId, searchText }) {
                     onClick={changeBookmark} />
             </div>
             <div className="post__body" dangerouslySetInnerHTML={{
-                __html:
-                    `<div class="highlight">
-           ${foundInBody(searchText)}
-           </div> ${body}`
+                __html: bodyHighlight
             }} />
         </div>
     )
